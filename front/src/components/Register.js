c8fbe2cf49b246845502c72c7f30ec18f0177170
Turpin Yannick <TurpinYa@gmail.com>
2022-05-13 14:23:14 +0200

    register

--- front/src/components/Register.js
+++ front/src/components/Register.js
@@ -1,5 +1,100 @@
+import { Form, Button, Input } from "antd";
+
 export default function Register() {
   return (
-    <div>Register</div>
-  )
+    <div>
+      {" "}
+      <Form
+        name="basic"
+        labelCol={{
+          span: 8,
+        }}
+        wrapperCol={{
+          span: 16,
+        }}
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+        autoComplete="off"
+      >
+        <Form.Item
+          label="Username"
+          name="username"
+          rules={[
+            {
+              required: true,
+              message: "Please input your username!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Password"
+          name="password"
+          rules={[
+            {
+              required: true,
+              message: "Please input your password!",
+            },
+          ]}
+        >
+          <Input.Password />
+        </Form.Item>
+
+        <Form.Item
+          label="age"
+          name="age"
+          rules={[
+            {
+              required: true,
+              message: "Please input your age!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Height"
+          name="height"
+          rules={[
+            {
+              required: true,
+              message: "Please input your height!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item> weight
+
+        <Form.Item
+          label="Weight"
+          name="weight"
+          rules={[
+            {
+              required: true,
+              message: "Please input your weight!",
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          wrapperCol={{
+            offset: 8,
+            span: 16,
+          }}
+        >
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
 }
