cf99137ce372aced5bfd6f1a94c085323f0e46e3
gaetan <gs99@hotmail.fr>
2022-05-20 09:42:38 +0200

    server

--- server/index.js
+++ server/index.js
@@ -1,17 +1,61 @@
 import express from "express"
-import data from "./data/user.json"
+import cors from "cors"
+import fs from "fs"
 
 const app = express()
 
-app.use(express.json())
+const fileUsers = "data/user.json"
 
-app.post('/produits', (req, res) => {
-    /*const {title, price, stock} = req.body
-    productService.addProduct(title, price, stock)
-    res.json({message: "produit ajouté"})*/
+app.use(express.json())
+app.use(cors({origin:"*"}))
+
+//get all users
+function getUsers(){
+    fs.readFile(fileUsers, (err, data) => {
+        if (err) throw err;
+        let users = JSON.parse(data)
+        console.log(users)
+        return users
+    });
+}
+
+//add a user
+function addUser(name, password, age, height, weight){
+    //console.log(getUsers())
+    const data = {
+        "id": getSizeUsers(),
+        "name": name,
+        "password": password,
+        "age": age,
+        "height": height,
+        "weight": weight
+    }
+    console.log(data)
+    fs.writeFile(fileUsers, JSON.stringify(data), (err) => {
+        if (err) throw err;
+        console.log('Data written to file');
+    });
+    //console.log(getUsers())
+}
+
+//get size of users
+function getSizeUsers(){
+    fs.readFile(fileUsers, (err, data) => {
+        if (err) throw err;
+        let users = JSON.parse(data)
+        //console.log(users)
+        return users.length
+    });
+}
+
+app.post('/user', (req, res) => {
+       const {name, password, age, height, weight} = req.body
+       addUser(name, password, age, height, weight)
+       res.json({message: true})
 })
 
 app.get('/users', (req, res) => {
+
     data.map((e,i)=>{
         res.json({message: "user found"})
     })
@@ -19,5 +63,8 @@ app.get('/users', (req, res) => {
 })
 
 app.listen(666,() => {
+
+    getUsers()
+
     console.log("server open at 666")
 })
